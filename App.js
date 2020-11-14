6576777c8bd50d47f0eb5f8e819bdd80c428484a
pgonzalesc <pedro281195@gmail.com>
2020-11-14 10:36:42 -0500

    Avance del User Profile

--- App.js
+++ App.js
@@ -8,107 +8,136 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
-  StatusBar,
+  Image,
+  Dimensions,
+  TextInput,
+  TouchableOpacity
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const {width} = Dimensions.get('window');
 
-const App: () => React$Node = () => {
+const App = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+      <View style={styles.container}>
+        {/* HEADER */}
+        <View style={styles.imageContainer}>
+          <Image 
+            source={{
+              uri: 'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80',
+            }}
+            style={{width: width/2.5, height: (width * 35) / 100, borderTopLeftRadius: 80,
+              borderTopRightRadius: 80, borderBottomLeftRadius: 80, borderBottomRightRadius: 80}}
+          />
+          <View style={styles.textProfileContainer}>
+            <TextInput
+              value='Lacey Fernandez'
+              style={styles.textProfile}
+            />
+            <TouchableOpacity style={styles.btnEditImageProfile}>
+            <Image 
+              source={{
+                uri: 'https://webstockreview.net/images/how-to-edit-png-images-14.png',
+              }}
+              style={{width: 20, height: 20}}
+            />
+            </TouchableOpacity>
           </View>
+        </View>
+        <ScrollView style={styles.cardProfileContainer}>
+            <View>
+              <Text style={styles.titleProfile}>USER PROFILE</Text>
+            </View>
+            <View>
+              <Text style={styles.labelCard}>User Name</Text>
+              <View style={styles.textContainer}>
+                <Image 
+                  source={{
+                    uri: 'https://www.pinclipart.com/picdir/middle/181-1814767_person-svg-png-icon-free-download-profile-icon.png',
+                  }}
+                  style={styles.iconCard}
+                />
+                <TextInput 
+                placeholder="Enter User Name"
+                style={styles.textCard}
+                />
+              </View>
+            </View>
         </ScrollView>
-      </SafeAreaView>
+      </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    backgroundColor: '#C4C2BF'
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  imageContainer: {
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: '#A141A9'
   },
-  body: {
-    backgroundColor: Colors.white,
+  textProfile: {
+    color: '#FFF'
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  textProfileContainer: {
+    //flex: 1, 
+    flexDirection: 'row'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  cardProfileContainer: {
+    // alignItems: 'center',
+    // justifyContent: 'flex-start',
+    flex: 1,
+    marginHorizontal: '4%',
+    marginBottom: '5%',
+    backgroundColor: '#FFF',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
   },
-  sectionDescription: {
-    marginTop: 8,
+  btnEditImageProfile: {
+    backgroundColor: '#e1e1e1',
+    height:40,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20, 
+    borderBottomLeftRadius: 20, 
+    borderBottomRightRadius: 20,
+    marginLeft:10
+  },
+  titleProfile: {
+    fontWeight: "bold",
     fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+    textAlign: "center",
   },
-  highlight: {
-    fontWeight: '700',
+  textContainer: {
+    flexDirection: 'row',
+    borderWidth: 2,
+    borderColor:"#e1e1e1",
+    borderRadius: 25,
+    paddingLeft: 5,
+    //paddingVertical: 5
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  textCard: {
+    marginLeft: 5
   },
+  iconCard: {
+    width: 30, 
+    height: 30,
+    marginTop: 10
+  },
+  labelCard: {
+    color: 'purple',
+    fontWeight: "bold",
+    fontSize: 14,
+    marginBottom: 5
+  }
 });
 
 export default App;
